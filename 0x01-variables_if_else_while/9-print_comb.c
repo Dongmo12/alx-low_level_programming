313290010f8618380c5c8cdbb56b1017273251f0
Armand Dongmo <dongmoarmand@gmail.com>
2022-10-15 23:07:00 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	int n = '0';
 	int a_to_f = 'a';
+
 	while (n <= '9') /*print 0-9*/
 	{
 		putchar(n);
@@ -20,7 +21,7 @@ int main(void)
 		putchar(a_to_f);
 		a_to_f++;
 	}
-	
+
 	putchar('\n');
 
 	return (0);
