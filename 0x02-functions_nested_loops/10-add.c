aac39c5e3be57e698c9103790451eeb85109f30d
Armand Dongmo <dongmoarmand@gmail.com>
2022-10-18 17:18:38 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,6 @@
 
 
 /**
- *
  * * add - computes the result of the addition of 2 integers
  *
  * *@a: right operand
@@ -14,10 +13,9 @@
  *
  * * Return: returns the result
  *
- * */
+ */
 
 int add(int a, int b)
-
 {
 
 	return (a + b);
