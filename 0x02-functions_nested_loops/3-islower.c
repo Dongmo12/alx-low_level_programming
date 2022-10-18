b2b34a51a55f7e71e1aea8bba8943b4e69debf41
Armand Dongmo <dongmoarmand@gmail.com>
2022-10-18 16:50:40 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,11 +11,9 @@
  * Return: returns 1 if the character is lower case and 0 otherwise
  */
 
-int _islower(int c)
-{
+int _islower(int c){
 
-	if (islower(c) != 0)
-	{
+	if (islower(c) != 0){
 
 		return (1);
 
