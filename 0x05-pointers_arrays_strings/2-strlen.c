221afc0694efc3f1d084bfdccfc729117ba25938
Armand Dongmo <dongmoarmand@gmail.com>
2022-10-25 08:35:30 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,15 @@
 
 
 /**
- *
  *  * _strlen - check the code for Holberton School students.
  *
  *   * @s: Inputted string pointer
  *
  *    * Return: Always 0.
  *
- *     */
+ */
 
 int _strlen(char *s)
-
 {
 
 		int i;
