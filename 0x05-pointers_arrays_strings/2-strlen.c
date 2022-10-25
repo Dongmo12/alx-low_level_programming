b5aef0124edd101d493b6ec55073d224240c4c31
Armand Dongmo <dongmoarmand@gmail.com>
2022-10-25 08:37:25 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,23 +11,16 @@
  *
  */
 
-int _strlen(char *s)
-{
+int _strlen(char * s) {
 
-		int i;
+  int i;
 
+  i = 0;
 
+  while (s[i] != '\0')
 
-			i = 0;
+    i++;
 
-
-
-				while (s[i] != '\0')
-
-							i++;
-
-
-
-					return (i);
+  return (i);
 
 }
