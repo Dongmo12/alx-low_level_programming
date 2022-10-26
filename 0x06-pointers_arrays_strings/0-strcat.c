3ad635bb46a3772e540f8d21cfb5f581fdc74922
Armand Dongmo <dongmoarmand@gmail.com>
2022-10-26 18:42:05 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,19 @@
  * Return: return value of dest
  */
 
-char * _strcat(char * dest, char * src) {
+char * _strcat(char * dest, char * src)
+{
   int i;
   int j;
 
   i = 0;
   j = 0;
 
-  while (dest[i] != '\0') {
+  while (dest[i] != '\0'){
     i++;
   }
 
-  while (src[j] != '\0') {
+  while (src[j] != '\0'){
     dest[i] = src[j];
     j++;
     i++;
